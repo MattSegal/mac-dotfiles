335392511c19057f1246fa3c06f8de77c4f0f6e8
Matthew Segal <matt.segal@themobian.com>
2024-11-20 21:35:12 +1100

    add symlinks

---
+++ link.sh
@@ -0,0 +1,4 @@
+# Setup symlinks
+ln -sf ~/code/dotfiles/.gitattributes ~/.gitattributes
+ln -sf ~/code/dotfiles/.gitignore.global ~/.gitignore
+ln -sf ~/code/dotfiles/.gitconfig ~/.gitconfig
--- setup.sh
+++ setup.sh
@@ -13,6 +13,24 @@ path=(
 )
 export PATH
 
+# Node version manager
+export NVM_DIR="$HOME/.nvm"
+
+# First try Homebrew NVM location, fall back to default NVM location
+if [ -n "$HOMEBREW_PREFIX" ] && [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ]; then
+    . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"  # This loads nvm from Homebrew
+    . "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion from Homebrew
+else
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
+# Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+eval "$(pyenv init -)"
+
+# Docker
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 # Matt
 . ~/code/dotfiles/matt.sh
@@ -25,15 +43,5 @@ if [[ -f ~/code/dotfiles/local.secret.sh ]]; then
     . ~/code/dotfiles/local.secret.sh
 fi
 
-# Docker
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
-# Pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-eval "$(pyenv init -)"
-
-# NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
